c7b9a8812febd9aab78b32d496359db538658af4
CodeBishop <srb@vcn.bc.ca>
2016-09-01 12:09:55 -0700

    Fixed user extraction to allow spaces in user description. Eliminated user ID extraction so remote computer will generate a unique ID during immigration.

--- extractPasswdData.sh
+++ extractPasswdData.sh
@@ -32,14 +32,14 @@ while read user_name; do
     user_result=$(grep "^$user_name:" /etc/passwd) # Entry line must start with username followed by a colon in order to match.
     
     # If a match was found then process it.
-    if [ ! -z $user_result ]; then
+    if [ ! -z "$user_result" ]; then
         # Split the user's entry by colons into an array.
         IFS=':' read -r -a array_of_field_data <<< "$user_result"
 
         # Convert the array to a set of fields.
         field_user=${array_of_field_data[index_user]}
         field_password=${array_of_field_data[index_password]}
-        field_uid=${array_of_field_data[index_uid]}
+        field_uid=""  # Migrated users will have new IDs generated for them at the other end.
         field_gid=${array_of_field_data[index_gid]}
         field_gecos=${array_of_field_data[index_gecos]}
         field_dir="/home"  # All migrated users share a home dir.
