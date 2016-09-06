b4509295f6b4af58e8707c8033c6faa375407d3a
CodeBishop <srb@vcn.bc.ca>
2016-09-06 15:59:33 -0700

    Debugged exportUsers.sh and set it up to remotely execute importUsers.sh

--- exportUsers.sh
+++ exportUsers.sh
@@ -4,11 +4,13 @@
 #   Check that the destination is reachable.
 #   Check that file copying succeeds before attempting newusers and chpasswd (maybe the files couldn't be created for permission reasons or maybe the transfer failed).
 #   Check that an ssh-key has already been installed to the remote machine and that ssh login is working. Consider making your script automate the ssh-keygen call if needed (maybe that should be a separate "install" script. Any automation of ssh-keygen should check that the specified remote user has superuser privileges.
+# Figure out how to embed file_user_data and file_pass_data variables into the remotely executed script.
 
 
-# Data files that will be passed between the two computers.
+# Files that will be passed to the remote machine.
 file_user_data="temp_passwd_data.txt"
 file_pass_data="temp_shadow_data.txt"
+script_remote_migration="importUsers.sh"
 remove_data_files_after_use=false
 
 # Check for correct number of command-line parameters.
@@ -30,6 +32,12 @@ if (( $EUID != 0 )); then
     exit 1
 fi
 
+# Check that a local copy of the importation script is present.
+if [! -f $script_remote_migration]; then
+    echo "Could not find the importation script: $script_remote_migration"
+    exit 1
+fi
+
 # Check for pre-authorized ssh access to remote machine.
 
 # Check that at least one user name from the given list was found among the users of this machine, if not then spit out an error to stderr. Then halt the script but give exit status of 0 since not matching any users should be a normal outcome.
@@ -45,44 +53,12 @@ echo "Copying user data to home folder of $2"
 scp $file_user_data $2:~
 scp $file_pass_data $2:~
 
-# Import the user data into the destination machine.
-# NOTE: User importing is implemented with a loop because the newusers command has a 3-year old bug that is yet to be fixed. Under hard-to-control circumstances the program will crash with an "invalid next size" error message. The useradd command was selected because adduser is interaction-oriented.
-ssh $2 << HERE
-# Indices of data members in an /etc/passwd entry.
-index_user=0
-index_password=1
-index_uid=2
-index_gid=3
-index_gecos=4
-index_dir=5
-index_shell=6
+echo "Copying remote migration script to home folder of $2"
+scp $script_remote_migration $2:~
 
-if [ $# -ne 1 ]; then
-        echo "A script for importing a list of users from a field in the same format as an /etc/passwd file."
-        echo "Usage: $0 <text file with user entries>"
-fi
-
-# Read in the new users and create their accounts.
-while read line; do
-    # Split the user's entry by colons into an array.
-    IFS=':' read -r -a array_of_field_data <<< "$line"
-
-    # Convert the array to a set of fields.
-    field_user=${array_of_field_data[index_user]}
-    field_password=${array_of_field_data[index_password]}
-    field_uid=${array_of_field_data[index_uid]}
-    field_gid=${array_of_field_data[index_gid]}
-    field_gecos=${array_of_field_data[index_gecos]}
-    field_dir=${array_of_field_data[index_dir]}
-    field_shell=${array_of_field_data[index_shell]}
-
-    # Create the new user (do not create a home directory).
-    useradd -u $field_uid -g $field_gid -c "$field_gecos" -M -s $field_shell $field_user
-done <$1
-
-# Assign passwords to the new users.
-chpasswd -e < shadow_output.txt
-HERE
+echo "Executing remote migration script on destination machine..."
+ssh -t $2 "./$script_remote_migration $file_user_data $file_pass_data"
+echo "Remote migration attempt complete."
 
 # Delete the temporary data files.
 if $remove_data_files_after_use; then
--- importUsers.sh
+++ importUsers.sh
@@ -6,6 +6,11 @@
 #   Double-check that useradd cannot create a duplicate entry if an attempt is made to add a user whose  is already present
 
 
+# Check that command arguments are correct.
+if [ $# -ne 2 ]; then
+        echo "A script for importing a list of users from a field in the same format as an /etc/passwd file."
+        echo "Usage: $0 <passwd file> <shadow file>"
+fi
 
 # Indices of data members in an /etc/passwd entry.
 index_user=0
@@ -16,13 +21,11 @@ index_gecos=4
 index_dir=5
 index_shell=6
 
-if [ $# -ne 1 ]; then
-        echo "A script for importing a list of users from a field in the same format as an /etc/passwd file."
-        echo "Usage: $0 <text file with user entries>"
-fi
-
 # Read in the new users and create their accounts.
+# NOTE: User importing is implemented with a loop because the newusers command has a bug that under hard-to-control circumstances will crash with an "invalid next size" error message. Documentation of this bug is available at: https://bugs.launchpad.net/ubuntu/+source/shadow/+bug/1266675
+echo "REMOTE: Adding new users."
 while read line; do
+    echo "REMOTE: Adding user $line"
     # Split the user's entry by colons into an array.
     IFS=':' read -r -a array_of_field_data <<< "$line"
 
@@ -39,6 +42,6 @@ while read line; do
     useradd -u $field_uid -g $field_gid -c "$field_gecos" -M -s $field_shell $field_user
 done <$1
 
-# Assign passwords to the new users.
-#chpasswd -e < shadow_output.txt
+echo "REMOTE: Migrating passwords."
+chpasswd -e < $2
 
