db8776103e1c1be05ae2570604f25ac61eb09894
CodeBishop <srb@vcn.bc.ca>
2016-09-06 12:28:26 -0700

    Added a script for experimenting with looping the useradd command

---
+++ importUsers.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This is a temporary script for working out how to use useradd in a loop (since newusers isn't working properly). It is intended that this code will be later integrated into exportUsers.sh to be executed remotely. At that time this script should probably be deleted from the project.
+
+
