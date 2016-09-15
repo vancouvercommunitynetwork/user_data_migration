6a7b3828735ca812e8f9348b6cf9550ca7748f36
CodeBishop <srb@vcn.bc.ca>
2016-09-15 12:03:11 -0700

    Switched some output to use here-document syntax and fixed a bug with exit status constants.

--- exportUsers.sh
+++ exportUsers.sh
@@ -21,9 +21,13 @@
 #       flock -n 200 || exitBecauseOfLockFailure
 #       echo $$ 1>&200
 #   Unlock at the end of the script and unlock temporary files.
-
+#   Add a remove_remote_temp_files_after_use flag and set it up to delete the temp files and remote copy of importUsers.sh.
 
  
+############################################################
+#              DECLARATIONS AND DEFINITIONS                #
+############################################################
+
 set -e  # Exit script on command failures.
 set -u  # Exit script on attempted use of undeclared variables.
 
@@ -33,33 +37,33 @@ file_pass_data="temp_shadow_data.txt"
 script_remote_migration="importUsers.sh"
 
 # Settings.
-remove_data_files_after_use=true
+remove_local_temp_files_after_use=true
 remove_import_script_after_use=false
 lock="/var/run/vcn_user_data_migration.lck"
 
 # Exit failure status values.
-EXIT_MULTIPLE_INSTANCE=1
-EXIT_BAD_PARAMETERS=2
-EXIT_INSUFFICIENT_USER_LEVEL=3
-EXIT_IMPORT_SCRIPT_NOT_FOUND=4
+readonly EXIT_MULTIPLE_INSTANCE=1
+readonly EXIT_BAD_PARAMETERS=2
+readonly EXIT_INSUFFICIENT_USER_LEVEL=3
+readonly EXIT_IMPORT_SCRIPT_NOT_FOUND=4
 
 exitBecauseOfLockFailure(){
     echo "Instance of $(basename "$0") already running!" 
-    exit EXIT_MULTIPLE_INSTANCE
+    exit $EXIT_MULTIPLE_INSTANCE
 }
 
 # Check for correct number of command-line parameters.
 checkParameterCount(){
-    if [ $1 -ne 2 ]; then
-        echo "Usage: $0 <user list file> <destination>"
-        echo
-        echo "Explanation"
-        echo "  This program requires a text file containing a newline-separated list of usernames and a network destination with pre-authorized ssh access."
-        echo
-        echo "Example:"
-        echo "  $0 bunch_of_users.txt root@192.168.1.257"
-        echo
-        exit EXIT_BAD_PARAMETERS
+    local parameterCount=$1
+    if [ $parameterCount -ne 2 ]; then cat <<HERE
+Usage: $0 [USER LIST FILE] [DESTINATION]
+Emigrate given users from this machine to a remote machine. The user list must be a text file containing a newline-separated list of usernames. The network destination needs to be pre-authorized for ssh access which can be done with ssh-keygen.
+
+Example:
+  $0 bunch_of_users.txt root@192.168.1.257
+
+HERE
+        exit $EXIT_BAD_PARAMETERS
     fi
 }
 
@@ -67,7 +71,7 @@ checkParameterCount(){
 checkUserLevel(){
     if (( $EUID != 0 )); then
         echo "ERROR: $(basename $0) must be called as sudo so it can read /etc/shadow."
-        exit EXIT_INSUFFICIENT_USER_LEVEL
+        exit $EXIT_INSUFFICIENT_USER_LEVEL
     fi
 }
 
@@ -75,39 +79,46 @@ checkUserLevel(){
 checkImportScriptIsPresent(){
     if [ ! -f "$script_remote_migration" ]; then
         echo "Could not find local copy of remote migration script: $script_remote_migration"
-        exit EXIT_IMPORT_SCRIPT_NOT_FOUND
+        exit $EXIT_IMPORT_SCRIPT_NOT_FOUND
     fi
 }
 
 
-# Test script execution prerequisites.
-checkParameterCount $#          # Was this called with correct parameter count?
-checkUserLevel                  # Was this called as sudo?
-checkImportScriptIsPresent      # Is a local copy of importUsers.sh present?
+############################################################
+#                MAIN BODY OF PROGRAM                      #
+############################################################
+
+# Check that migration prerequisites are met.
+checkParameterCount $#          # The parameter count should be correct.
+checkUserLevel                  # This script must be called as root.
+checkImportScriptIsPresent      # A local copy of importUsers.sh must be present.
+
+# Name program parameters.
+user_list=$1
+destination=$2
 
 # Proceed with migration.
 echo "Extracting local user data to: $file_user_data"
-./extractPasswdData.sh < $1 > $file_user_data
+./extractPasswdData.sh < $user_list > $file_user_data
 
 echo "Extracting local user passwords to: $file_pass_data"
-./extractShadowData.sh < $1 > $file_pass_data
+./extractShadowData.sh < $user_list > $file_pass_data
 
-echo "Copying user data to home folder of $2"
-scp $file_user_data $2:~
-scp $file_pass_data $2:~
+echo "Copying user data to home folder of $destination"
+scp $file_user_data $destination:~
+scp $file_pass_data $destination:~
 
-echo "Copying remote migration script to home folder of $2"
-scp $script_remote_migration $2:~
+echo "Copying remote migration script to home folder of $destination"
+scp $script_remote_migration $destination:~
 
 echo "Attempting to execute remote migration script on destination machine..."
-ssh -t $2 "./$script_remote_migration $file_user_data $file_pass_data"
+ssh -t $destination "./$script_remote_migration $file_user_data $file_pass_data"
 echo "Remote migration attempt complete."
 
 # Delete the temporary data files.
-if "$remove_data_files_after_use"; then
+if "$remove_local_temp_files_after_use"; then
     echo "Removing temporary files from local machine."
     rm $file_user_data
     rm $file_pass_data
-    # Delete the remote copies.
 fi
 
