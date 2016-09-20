3c2ecec7e3f9030e9a6171d1a3e780c26a5a1209
CodeBishop <srb@vcn.bc.ca>
2016-09-20 13:43:21 -0700

    Finalized working version of migrate.sh.

--- migrate.sh
+++ migrate.sh
@@ -26,14 +26,24 @@ while read user_name; do
 
     # If user entries were found extract data from them.
     if [ ! -z "$passwd_result" ] && [ ! -z "$shadow_result" ]; then
+        # /etc/passwd fields are indexed as username:password:userID:groupID:gecos:homeDir:shell
         IFS=':' read -r -a passwd_fields <<< "$passwd_result"
+        # /etc/shadow fields are indexed as username:password:lastchanged:minimum:maximum:warn:inactive:expire
         IFS=':' read -r -a shadow_fields <<< "$shadow_result"
+
         echo Migrating user: ${passwd_fields[0]}
-        # /etc/passwd entries are indexed as username:password:userID:groupID:gecos:homeDir:shell
-        ssh -n $1 /usr/sbin/useradd -u ${passwd_fields[2]} -g ${passwd_fields[3]} -c \"${passwd_fields[4]}\" -M -s "/sbin/nologin" ${passwd_fields[0]}
-        # /etc/shadow are entries indexed as username:password
+
+        # Remotely add user accounts (ssh will halt loop without -n). See above for index meanings. Users are not intended to have shell login.
+        ssh -n $1 /usr/sbin/useradd -p \'${shadow_fields[1]}\' -g ${passwd_fields[3]} -c \"${passwd_fields[4]}\" -M -s "/sbin/nologin" ${passwd_fields[0]}
     fi
 done <"$2"
 
 exit 0
 
+
+Current ideas
+    figure out how the useradd -p is supposed to work 
+    call chpasswd -e and find someway to feed a string into it as if it were a file
+        this must be able to work because it works for exportUsers.sh. "chpass -e < tempShadow.txt" creates an account that I can then "su - test8" into (from test9) and it will prompt for password and the password works.
+        <<< doesn't seem to work the way I thought it does
+        
