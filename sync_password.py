887dcaa2aa1242fa71dd527362e149f5eaefa760
Sebastian Bejm <125sbejm@gmail.com>
2025-10-16 13:38:24 -0700

    remove commented out old code

--- sync_password.py
+++ sync_password.py
@@ -5,23 +5,6 @@ import dbm
 USERS_LIST_FILE = "test_users_new_pass.txt"
 CACHE_FILE = "user_cache"
 
-def get_users_list(user_list_file):
-    # Store usernames/passwords for reference in a dictionary
-    users = {}
-
-    try:
-        with open(user_list_file, 'r') as file:
-            for line in file:
-                user = line.strip()
-                if user:
-                    username, password = user.split(":")
-                    # Store username and password
-                    users[username] = password 
-            return users
-    except (IOError, OSError) as e:
-        print(f"Error reading user list file: {e}", file=sys.stderr)
-        sys.exit()
-
 def get_password(username):
     # Returns the password of a user if the username parameter matches with an existing user
     try:
@@ -45,21 +28,9 @@ def get_cached_password(username):
                 cache_pass_str = cache[username].decode('utf-8')
                 return cache_pass_str.strip()
     except (IOError, OSError) as e:
-        print(f"Could not open the dbm file: {e}", file=sys.stderr)
+        print(f"Error reading the cache file: {e}", file=sys.stderr)
         sys.exit()
     return None
-
-def check_user_password_match(users_list, user_to_search):
-    # Returns whether there is a match with user's password in both the user list and the cache
-    try:
-        with dbm.open(CACHE_FILE, 'r') as cache:
-            # Have to convert the username string to bytes for the check since the cache is in bytes
-            if bytes(user_to_search, 'utf-8') in cache.keys():
-                cache_pass_str = cache[user_to_search].decode('utf-8') # Convert the password that is in bytes to string
-                return users_list[user_to_search].strip() == cache_pass_str.strip()
-    except (IOError, OSError) as e:
-        print(f"Could not open the dbm file: {e}", file=sys.stderr)
-        sys.exit()
 
 def update_cached_password(username, new_password):
     # Updates the dbm cache for a single user with a new password, and outputs the updated password
@@ -68,33 +39,22 @@ def update_cached_password(username, new_password):
             cache[username] = new_password
             print(new_password)
     except (IOError, OSError) as e:
-        print(f"Could not open the dbm file: {e}", file=sys.stderr)
+        print(f"Error writing to the cache file: {e}", file=sys.stderr)
         sys.exit()
 
 def main():
-    # Read in and store the users list as a dict for reference
-    # users_list = get_users_list(USERS_LIST_FILE)
-
-    # Update user cache
-    '''for user_to_search in users_to_search:
-        # Only update passwords in user cache if there is no match
-        if check_user_password_match(users_list, user_to_search) is False:
-            update_cached_password(user_to_search, users_list[user_to_search])'''
-
     # Process standard input from command line
     users_to_search = []
     for line in sys.stdin:
         user = line.strip()
         users_to_search.append(user)
 
+    # Loop through users and update passwords as necessary
     for user_to_search in users_to_search:
-        new_password = get_password(user_to_search)
-        #print("new password: " + new_password)
+        current_password = get_password(user_to_search)
         cached_password = get_cached_password(user_to_search)
-        #print("cached password: " + cached_password)
-        if new_password != cached_password and cached_password is not None:
-            update_cached_password(user_to_search, new_password)
-
+        if current_password != cached_password and cached_password is not None:
+            update_cached_password(user_to_search, current_password)
 
 if __name__ == "__main__":
     main()
